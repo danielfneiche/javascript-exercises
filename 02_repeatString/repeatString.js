5b97abf47486676eaf93e74b397982797ee273ba
Daniel Fneiche <48098584+danielfneiche@users.noreply.github.com>
2024-02-28 22:03:48 +0000

    Completed repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(str, numb){
+    if(numb < 0) {
+        return "ERROR";
+    } 
+    let repeatedString = "";
+    for(i = 0; i < numb; i++) {
+        repeatedString += str;
+    }
+    return repeatedString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
