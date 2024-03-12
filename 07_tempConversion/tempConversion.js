c61d79149153229f5df1ed97a56ac60bc10fbe7f
Daniel Fneiche <48098584+danielfneiche@users.noreply.github.com>
2024-03-12 16:40:36 +0000

    Completed tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return fahrenheit = Math.round((5/9*(fahrenheit - 32)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  celsius = (9/5) * celsius + 32;
+  return celsius = Math.round(celsius * 10) / 10;
 };
 
 // Do not edit below this line
